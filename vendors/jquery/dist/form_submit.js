3a4792fe1d8df2166aeddd2cc0c983c31fd00586
VM-Sharukh <156588267+VM-Sharukh@users.noreply.github.com>
2024-01-15 21:54:43 +0530

    Add files via upload

--- vendors/jquery/dist/form_submit.js
+++ vendors/jquery/dist/form_submit.js
@@ -1,30 +1,1 @@
-
-	$(function(){
-		SubmitForm('LoginForm');
-		SubmitForm('ManageUserForm');
-	});
-
-	function SubmitForm(FormID){
-		var myform = $("#"+FormID);
-		  $(myform).ajaxForm({
-			  beforeSend: function() {
-				  $('.error_msg').html('');
-				  $('.success_message').html('<br><div class="alert alert-info">Data analyzing...please wait...</div>');
-			  },
-			  complete: function(response){
-				// console.log(response);return false;
-				var temp = JSON.parse(response.responseText);
-				if(temp.status == 'success'){
-					$('.success_message').show().html(temp.message);
-					myform.resetForm();
-					window.location.href= temp.redirect;
-				}
-				else if(temp.status == 'error'){
-				  $.each(temp.errors, function(key, val){
-					  $('.success_message').html('');
-					  $('.'+key).html(val);
-				  });
-				}
-			  }
-		  });  
-	}
+	$(function(){		SubmitForm('LoginForm');		SubmitForm('ManageUserForm');	});	function SubmitForm(FormID){		var myform = $("#"+FormID);		  $(myform).ajaxForm({			  beforeSend: function() {				  $('.error_msg').html('');				  $('.success_message').html('<br><div class="alert alert-info">Data analyzing...please wait...</div>');			  },			  complete: function(response){				// console.log(response);return false;				var temp = JSON.parse(response.responseText);				if(temp.status == 'success'){					$('.success_message').show().html(temp.message);					myform.resetForm();					window.location.href= temp.redirect;				}				else if(temp.status == 'error'){				  $.each(temp.errors, function(key, val){					  $('.success_message').html('');					  $('.'+key).html(val);				  });				}			  }		  });  	}
