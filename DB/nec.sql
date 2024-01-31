cab0399c16edf4c8dfad27376e59d7f03a49acbd
VM-Sharukh <156588267+VM-Sharukh@users.noreply.github.com>
2024-01-31 16:01:22 +0530

    Update nec.sql

--- DB/nec.sql
+++ DB/nec.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2024 at 02:32 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jan 31, 2024 at 11:30 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `nec`
 --
-CREATE DATABASE IF NOT EXISTS `nec` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `nec`;
 
 -- --------------------------------------------------------
 
@@ -43,48 +41,19 @@ CREATE TABLE `users` (
   `LastUpdated` datetime NOT NULL,
   `LastLoggedIn` datetime DEFAULT NULL,
   `LastLoggedOut` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`UserID`, `FullName`, `EmailID`, `Password`, `Gender`, `UserRole`, `UserStatus`, `UserProfileImage`, `CreatedAt`, `LastUpdated`, `LastLoggedIn`, `LastLoggedOut`) VALUES
-(1, 'Admin', 'admin@gmail.com', '9870987666', 1, 1, 1, 'Admin.jpg', '2024-01-12 17:25:17', '2024-01-12 17:25:17', '2024-01-14 18:37:27', '2024-01-14 18:37:16'),
-(2, 'Mohd Sharukh Varyala', 'vsharukh@gmail.com', '7208136767', 1, 2, 1, '17052058724IutKrvYgLo!eVa.png', '2024-01-12 17:25:17', '2024-01-14 18:47:46', '2024-01-14 18:43:09', '2024-01-14 18:36:25'),
-(3, 'Mohammed Shaheer', 'mohammedshaheer@gmail.com', '7234572822', 1, 2, 1, '1705237662wrileBjT!c1X-6A.jpg', '2024-01-13 13:15:45', '2024-01-14 18:47:30', NULL, NULL),
-(4, 'Afreen', 'afreen.dattari@gmail.com', '7977654876', 2, 2, 1, '1705237691wbomygiN3jSkI51.jpg', '2024-01-13 14:49:41', '2024-01-14 18:47:32', NULL, NULL),
-(5, 'Ameerah', 'ameerah@gmail.com', '9876543210', 2, 2, 1, '1705229992F80Ie!zYUsgVlbG.png', '2024-01-14 16:29:52', '2024-01-14 18:47:33', NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_logged_in_details`
---
-
-DROP TABLE IF EXISTS `user_logged_in_details`;
-CREATE TABLE `user_logged_in_details` (
-  `ULIDID` bigint(11) NOT NULL,
-  `UserID` bigint(11) NOT NULL,
-  `LogginDateTime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_page_visited_logs`
---
-
-DROP TABLE IF EXISTS `user_page_visited_logs`;
-CREATE TABLE `user_page_visited_logs` (
-  `UPVLID` bigint(11) NOT NULL,
-  `PageURL` varchar(55) NOT NULL,
-  `ReferrerURL` varchar(55) NOT NULL,
-  `UserIPAddress` varchar(55) NOT NULL,
-  `UserAgent` varchar(55) NOT NULL,
-  `UserID` bigint(11) NOT NULL,
-  `AddedDateTime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(1, 'Admin', 'admin@gmail.com', 'Sm6Fz/FR4KtObw==', 1, 1, 1, 'Admin.jpg', '2024-01-12 17:25:17', '2024-01-12 17:25:17', '2024-01-31 15:48:14', '2024-01-31 15:58:17'),
+(2, 'Mohd Sharukh Varyala', 'vsharukh@gmail.com', 'Sm6Fz/FR4KtObw==', 1, 2, 1, '17052058724IutKrvYgLo!eVa.png', '2024-01-12 17:25:17', '2024-01-31 15:57:02', '2024-01-14 18:43:09', '2024-01-14 18:36:25'),
+(3, 'Mohammed Shaheer', 'mohammedshaheer@gmail.com', 'Sm6Fz/FR4KtObw==', 1, 2, 1, '1705237662wrileBjT!c1X-6A.jpg', '2024-01-13 13:15:45', '2024-01-31 15:57:21', NULL, NULL),
+(4, 'Afreen', 'afreen.dattari@gmail.com', 'QmSBzfFT5KFGbw==', 2, 2, 1, '1705237691wbomygiN3jSkI51.jpg', '2024-01-13 14:49:41', '2024-01-31 15:57:37', '2024-01-31 15:58:23', '2024-01-31 15:58:24'),
+(5, 'Ameerah', 'ameerah@gmail.com', 'Sm6Fz/FR4KtObw==', 2, 2, 1, '1705229992F80Ie!zYUsgVlbG.png', '2024-01-14 16:29:52', '2024-01-31 15:57:57', NULL, NULL),
+(6, 'Shafin raza', 'shafinraza@gmail.com', 'Sm6Fz/FR4KtObw==', 1, 2, 1, '1706696343WEsC63B4ThvMq!X.jpeg', '2024-01-31 15:49:03', '2024-01-31 15:58:09', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -97,18 +66,6 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`UserID`);
 
 --
--- Indexes for table `user_logged_in_details`
---
-ALTER TABLE `user_logged_in_details`
-  ADD PRIMARY KEY (`ULIDID`);
-
---
--- Indexes for table `user_page_visited_logs`
---
-ALTER TABLE `user_page_visited_logs`
-  ADD PRIMARY KEY (`UPVLID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -116,19 +73,7 @@ ALTER TABLE `user_page_visited_logs`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `user_logged_in_details`
---
-ALTER TABLE `user_logged_in_details`
-  MODIFY `ULIDID` bigint(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user_page_visited_logs`
---
-ALTER TABLE `user_page_visited_logs`
-  MODIFY `UPVLID` bigint(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `UserID` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
